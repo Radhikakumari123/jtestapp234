c76224d81fb5a65253c5af148f81fec84f503056
easylearningjyoti <jyoti@easylearning.guru>
2016-09-29 15:00:40 +0530

    first commit

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,6 +77,5 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-GA.tracker = "UA-84837580-2
-"
+#GA.tracker = "UA-84963861-1"
 end
